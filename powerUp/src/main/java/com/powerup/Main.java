b3d7e7591a2cba1ee918fd705f6793b7ed6c34b9
lewisohn <oliver.lewisohn@helsinki.fi>
2014-01-23 01:45:56 +0200

    Update

---
+++ powerUp/src/main/java/com/powerup/Main.java
@@ -0,0 +1,15 @@
+package com.powerup;
+import com.powerup.logic.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Testing powerUp" );
+        Game game = new Game();
+    }
+}
